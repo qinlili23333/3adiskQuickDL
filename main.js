7bf1d20ab470d1b3a163998b74b83e44f1cee0d8
琴梨梨 <yeyaohua2010@163.com>
2021-02-16 11:14:21 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         3adisk秒出下载
+// @namespace    http://qinlili.bid
+// @version      0.1
+// @description  下载按钮秒出无需等待
+// @author       琴梨梨
+// @match        http://www.3adisk.com/down_*.shtml
+// @icon         http://www.3adisk.com/favicon.ico
+// @grant        none
+// @run-at        document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    x636=1;
+    countdown();
+})();
